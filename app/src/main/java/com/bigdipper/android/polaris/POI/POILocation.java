3a4b340b9685acab21b227adb7ba22db01d71d1f
JIW00NG <ipkjw2@gmail.com>
2021-05-24 21:43:10 +0900

    검색 구현 및 검색 요소 선택 추가

--- app/src/main/java/com/bigdipper/android/polaris/POI/POILocation.java
+++ app/src/main/java/com/bigdipper/android/polaris/POI/POILocation.java
@@ -24,55 +24,27 @@ public class POILocation {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getRadius() {
         return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
     }
 
     public String getBizName() {
         return bizName;
     }
 
-    public void setBizName(String bizName) {
-        this.bizName = bizName;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        address = address;
     }
 
     public String getRoadAddress() {
         return roadAddress;
     }
 
-    public void setRoadAddress(String roadAddress) {
-        this.roadAddress = roadAddress;
-    }
-
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 }
--- app/src/main/java/com/bigdipper/android/polaris/ui/nearby/NearbyFragment.java
+++ app/src/main/java/com/bigdipper/android/polaris/ui/nearby/NearbyFragment.java
@@ -7,16 +7,22 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bigdipper.android.polaris.POI.POILocation;
@@ -49,7 +55,12 @@ public class NearbyFragment extends Fragment implements TMapGpsManager.onLocatio
 
     // View
     EditText searchText;
-    Button searchBtn;
+    Button cancelBtn;
+    Button backBtn;
+    Button directionBtn;
+
+    // Dynamic View
+    LinearLayout searchResultLayout;
 
     static double longitude, latitude;
 
@@ -58,8 +69,81 @@ public class NearbyFragment extends Fragment implements TMapGpsManager.onLocatio
         View root = inflater.inflate(R.layout.fragment_nearby, container, false);
 
         // View
-        searchText = root.findViewById(R.id.edit_search);
-        searchBtn = root.findViewById(R.id.btn_search);
+        searchText = root.findViewById(R.id.et_search);
+        cancelBtn = root.findViewById(R.id.btn_nearby_cancel);
+        backBtn = root.findViewById(R.id.btn_nearby_back);
+        directionBtn = root.findViewById(R.id.btn_direction);
+
+        // Dynamic View
+        searchResultLayout = (LinearLayout) root.findViewById(R.id.searchList);
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchText.setText("");
+                searchText.clearFocus();
+                cancelBtn.setVisibility(View.GONE);
+            }
+        });
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchResultLayout.removeAllViews();
+                searchResultLayout.setVisibility(View.GONE);
+                searchText.clearFocus();
+                searchText.setText("");
+                backBtn.setVisibility(View.GONE);
+                cancelBtn.setVisibility(View.GONE);
+                InputMethodManager mInputMethodManager = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                mInputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+        directionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchResultLayout.removeAllViews();
+                cancelBtn.setVisibility(View.VISIBLE);
+                try {
+                    String searchKeyword = searchText.getText().toString();
+
+                    URL searchUrl = new URL("https://apis.openapi.sk.com/tmap/pois?appKey=" + API_Key + "&version=1&searchKeyword=" + searchKeyword + "&searchtypCd=R&radius=0&centerLon=" + longitude + "&centerLat=" + latitude);
+                    Runnable search = new SearchPOI(searchUrl);
+                    Thread searchThread = new Thread(search);
+                    searchThread.start();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                searchResultLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        searchText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    backBtn.setVisibility(View.VISIBLE);
+                }else{
+
+                }
+            }
+        });
 
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 //            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -123,22 +207,6 @@ public class NearbyFragment extends Fragment implements TMapGpsManager.onLocatio
                 1,
                 mLocationListener);
 
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try {
-                    String searchKeyword = searchText.getText().toString();
-
-                    URL searchUrl = new URL("https://apis.openapi.sk.com/tmap/pois?appKey=" + API_Key + "&version=1&searchKeyword=" + searchKeyword + "&searchtypCd=R&radius=0&centerLon=" + longitude + "&centerLat=" + latitude);
-                    Runnable search = new SearchPOI(searchUrl);
-                    Thread searchThread = new Thread(search);
-                    searchThread.start();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
 
         return root;
     }
@@ -187,17 +255,26 @@ public class NearbyFragment extends Fragment implements TMapGpsManager.onLocatio
 
                                 POILocation poiLocation = new POILocation(resultName, resultRadius, resultLowerBizName, resultFullAddressRoad, resultFullAddress, resultCenterLat, resultCenterLon);
                                 searchResult.add(poiLocation);
-                                System.out.println("--------------------------------------------------------------------------------------------------");
-                                System.out.println("이름 : " + searchResult.get(0).getName());
-                                System.out.println("거리: " + searchResult.get(0).getRadius());
-                                System.out.println("종류: " + searchResult.get(0).getBizName());
-                                System.out.println("도로명 주소: " + searchResult.get(0).getRoadAddress());
-                                System.out.println("지번 주소: " + searchResult.get(0).getAddress());
-                                System.out.println("위도: " + searchResult.get(0).getLatitude());
-                                System.out.println("경도: " + searchResult.get(0).getLongitude());
+                                getActivity().runOnUiThread((new Runnable(){
+
+                                    @Override
+                                    public void run() {
+                                        showSearchResult(searchResult, result.length());
+
+                                    }
+                                }));
+//                                System.out.println("--------------------------------------------------------------------------------------------------");
+//                                System.out.println("이름 : " + searchResult.get(0).getName());
+//                                System.out.println("거리: " + searchResult.get(0).getRadius());
+//                                System.out.println("종류: " + searchResult.get(0).getBizName());
+//                                System.out.println("도로명 주소: " + searchResult.get(0).getRoadAddress());
+//                                System.out.println("지번 주소: " + searchResult.get(0).getAddress());
+//                                System.out.println("위도: " + searchResult.get(0).getLatitude());
+//                                System.out.println("경도: " + searchResult.get(0).getLongitude());
                             }
                             //System.out.println(data);
                             content.append(data); // 검색 결과: data
+
                         }
                     }
 
@@ -210,6 +287,54 @@ public class NearbyFragment extends Fragment implements TMapGpsManager.onLocatio
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    private void showSearchResult(ArrayList<POILocation> searchResult, int size){
+
+        for(int i = 0; i < size; i++){
+            LinearLayout searchElementLayout = new LinearLayout(getContext());
+            LinearLayout searchResultNameLayout = new LinearLayout(getContext());
+            LinearLayout searchResultAddressLayout = new LinearLayout(getContext());
+            searchElementLayout.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_bottom_stroke));
+
+            searchElementLayout.setOrientation(LinearLayout.VERTICAL);
+            searchElementLayout.setId(i);
+            int id = i;
+            searchElementLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    searchResultLayout.setVisibility(View.GONE);
+                    tMapView.setLocationPoint(searchResult.get(id).getLongitude(), searchResult.get(id).getLatitude());
+                    tMapView.setCenterPoint(searchResult.get(id).getLongitude(), searchResult.get(id).getLatitude());
+                }
+            });
+
+            searchResultNameLayout.setOrientation(LinearLayout.HORIZONTAL);
+            searchResultAddressLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView resultName = new TextView(getContext());
+            TextView resultRadius = new TextView(getContext());
+            TextView resultBizName = new TextView(getContext());
+            TextView resultRoadAddress = new TextView(getContext());
+            TextView resultAddress = new TextView(getContext());
+            TextView resultLatitude = new TextView(getContext());
+            TextView resultLongitude = new TextView(getContext());
+
+            resultName.setText(searchResult.get(i).getName());
+            resultRadius.setText(String.valueOf(searchResult.get(i).getRadius()));
+            resultBizName.setText(searchResult.get(i).getBizName());
+            resultRoadAddress.setText(searchResult.get(i).getRoadAddress());
+
+            searchResultNameLayout.addView(resultName);
+            searchResultNameLayout.addView(resultBizName);
+            searchResultAddressLayout.addView(resultRoadAddress);
+            searchResultAddressLayout.addView(resultRadius);
+
+            searchElementLayout.addView(searchResultNameLayout);
+            searchElementLayout.addView(searchResultAddressLayout);
+
+            searchResultLayout.addView(searchElementLayout);
         }
     }
 
