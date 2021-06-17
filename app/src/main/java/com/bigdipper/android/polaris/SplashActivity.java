02565b236f1f90886ff8e7fd933689056fcc9fc1
JooHyung <yamiblack95@gmail.com>
2021-06-17 22:17:48 +0900

    SAP setting

--- app/src/main/java/com/bigdipper/android/polaris/SplashActivity.java
+++ app/src/main/java/com/bigdipper/android/polaris/SplashActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
@@ -16,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends Activity {
     private final int SPLASH_TIME = 1500;
+    private long backKeyPressedTime = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +31,28 @@ public class SplashActivity extends Activity {
                 if (FirebaseAuth.getInstance().getCurrentUser() == null) {
                     startActivity(new Intent((getApplication()), LoginActivity.class));
                 } else {
-                    startActivity(new Intent((getApplication()), MainActivity.class));
+//                    startActivity(new Intent((getApplication()), MainActivity.class));
+                    startActivity(new Intent((getApplication()), MessageActivity.class));
                 }
             }
         }, SPLASH_TIME);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 1000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (System.currentTimeMillis() <= backKeyPressedTime + 1000) {
+            super.onBackPressed();
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
 
     }
 
